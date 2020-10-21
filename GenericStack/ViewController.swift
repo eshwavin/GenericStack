f79c12b97facf34f8d0db1e7232c14fde4db6159
Srivinayak Chaitanya Eshwa <eshwavin@hotmail.com>
2020-10-21 17:04:10 +0530

    Initial Commit

---
+++ GenericStack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GenericStack
+//
+//  Created by Srivinayak Chaitanya Eshwa on 21/10/20.
+//  Copyright © 2020 Srivinayak Chaitanya Eshwa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
