f02708bf64189ad3843eed8a4b72e26c69ab62d4
Srivinayak Chaitanya Eshwa <eshwavin@hotmail.com>
2021-01-09 17:39:17 +0530

    Added functionality to UIViewController extension

--- GenericStack/Extensions/UIViewController+Extensions.swift
+++ GenericStack/Extensions/UIViewController+Extensions.swift
@@ -10,6 +10,8 @@ import UIKit
 
 extension UIViewController {
     
+    // MARK:- Presenting ViewControllers
+    
     func push(_ viewController: UIViewController) {
         navigationController?.pushViewController(viewController, animated: true)
     }
@@ -30,6 +32,25 @@ extension UIViewController {
     
     func popToRootViewController() {
         navigationController?.popToRootViewController(animated: true)
+    }
+    
+    func present(_ viewController: UIViewController, requiresFullScreen: Bool = true, completion: (() -> ())? = nil) {
+        viewController.modalPresentationStyle = requiresFullScreen ? .fullScreen : .automatic
+        present(viewController, animated: true, completion: completion)
+    }
+    
+    // MARK:- Navigation Bar
+    
+    func hideNavigationBar(animated: Bool) {
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    func showNavigationBar(animated: Bool) {
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
+    func hideBackButtonTitle() {
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
     }
     
 }
